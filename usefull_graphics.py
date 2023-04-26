d0358bcbbfb6a0883a2b85a0d5daa7458af2075b
lukedam <lukedamlab@gmail.com>
2023-04-26 22:40:53 +0300

    "sync"

--- usefull_graphics.py
+++ usefull_graphics.py
@@ -2,16 +2,17 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.style as mplstyle
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
+from matplotlib.colors import Normalize, ListedColormap
 
 def plot_2d_in_row(figures, titles=None, xlabels=None, ylabels=None,
                         show_colorbar=False, figsize=(6, 6), font_size=12, 
                         nrows=1, normalize=None, cmap='Spectral'):
     num_figures = len(figures)
     ncols = (num_figures + nrows - 1) // nrows
-
-    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    mplstyle.use('fast')
+    fig, axs = plt.plot(figsize=figsize)
 
     plt.rcParams.update({'font.size': font_size})
 
@@ -39,56 +40,87 @@ def plot_2d_in_row(figures, titles=None, xlabels=None, ylabels=None,
     return fig
 
 
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from scipy import interpolate
-
-
-def plot_3d_in_row(figures, titles=None, xlabels=None, ylabels=None, zlabels=None,
-                   show_colorbar=False, figsize=(6, 6), font_size=12, nrows=1, 
-                   normalize=None, exclude=None, cmap='Spectral', edgecolor=None):
+def plot_3d_voxels(figure, title=None, xlabel=None, ylabel=None, zlabel=None, 
+                   show_colorbar=False, font_size=12, normalize=None, exclude=None, cmap='Spectral'):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x_size, y_size, z_size = figure.shape
     
-    num_figures = len(figures)
-    ncols = (num_figures + nrows - 1) // nrows
-
-    fig, axs = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize, subplot_kw={'projection': '3d'})
-
     plt.rcParams.update({'font.size': font_size})
-    
     # Ensure that a valid colormap is passed
     try:
-        cmap_obj = cm.get_cmap(cmap)
+        cmap_obj = plt.get_cmap(cmap)
     except ValueError:
         raise ValueError("Invalid colormap: {}".format(cmap))
+    
+    data = figure
+    if exclude is not None:
+        data = np.where(abs(data) < exclude, np.nan, data)
+    if normalize is not None:
+        norm = Normalize(vmin=normalize[0], vmax=normalize[1])
+    else:
+        norm = Normalize(vmin=np.nanmin(data), vmax=np.nanmax(data))
+    if len(data.shape) == 3:
+        # Create a voxel grid and set the facecolors and edgecolors based on the normalized data
+        ax.voxels(data, facecolors=cmap_obj(norm(data)), edgecolor='k')
+    else:
+        raise ValueError("Data must be 3D array")
+    if title is not None:
+        ax.set_title(title)
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+    if zlabel is not None:
+        ax.set_zlabel(zlabel)
+    if show_colorbar:
+        # Create a colorbar for the normalized data
+        mappable = plt.cm.ScalarMappable(norm=norm, cmap=cmap_obj)
+        mappable.set_array(data)
+        plt.colorbar(mappable)
+    
+    plt.subplots_adjust(wspace=0.05, hspace=0.05)
+    return fig
 
-    for i, ax in enumerate(np.ravel(axs)):
-        if i >= num_figures:
-            break
-        data = figures[i]
-        if exclude is not None:
-            data = np.where(data < exclude, np.nan, data)
-        if normalize is not None:
-            norm = plt.Normalize(vmin=normalize[0], vmax=normalize[1])
-            colors = cmap_obj(norm(data.flatten()))
-        else:
-            norm = plt.Normalize(vmin=np.min(data), vmax=np.max(data))
-            colors = cmap_obj(data.flatten())
-        if len(data.shape) == 3:
-            filled = data > 0  # Create a boolean array indicating which voxels are filled
-            surf = ax.voxels(filled, facecolors=colors.reshape(data.shape[0], data.shape[1], data.shape[2], -1), edgecolor=edgecolor)
-        else:
-            raise ValueError("Data must be 3D array")
-        if titles is not None:
-            ax.set_title(titles[i])
-        if xlabels is not None:
-            ax.set_xlabel(xlabels[i])
-        if ylabels is not None:
-            ax.set_ylabel(ylabels[i])
-        if zlabels is not None:
-            ax.set_zlabel(zlabels[i])
-        if show_colorbar:
-            fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap_obj), ax=ax)
+
+def plot_3d_voxels_gpu(figure, title=None, xlabel=None, ylabel=None, zlabel=None, 
+                       show_colorbar=False, font_size=12, normalize=None, exclude=None, cmap='Spectral'):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x_size, y_size, z_size = figure.shape
+    
+    plt.rcParams.update({'font.size': font_size})
+    # Ensure that a valid colormap is passed
+    try:
+        cmap_obj = plt.get_cmap(cmap)
+    except ValueError:
+        raise ValueError("Invalid colormap: {}".format(cmap))
+    
+    data = cp.asarray(figure) # convert input array to CuPy array
+    if exclude is not None:
+        data = cp.where(cp.absolute(data) < exclude, cp.nan, data)
+    if normalize is not None:
+        norm = Normalize(vmin=normalize[0], vmax=normalize[1])
+    else:
+        norm = Normalize(vmin=cp.nanmin(data), vmax=cp.nanmax(data))
+    if len(data.shape) == 3:
+        # Create a voxel grid and set the facecolors and edgecolors based on the normalized data
+        ax.voxels(data.get(), facecolors=cmap_obj(norm(data.get())), edgecolor='k') # convert CuPy array back to NumPy for plotting
+    else:
+        raise ValueError("Data must be 3D array")
+    if title is not None:
+        ax.set_title(title)
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+    if zlabel is not None:
+        ax.set_zlabel(zlabel)
+    if show_colorbar:
+        # Create a colorbar for the normalized data
+        mappable = plt.cm.ScalarMappable(norm=norm, cmap=cmap_obj)
+        mappable.set_array(data.get()) # convert CuPy array back to NumPy for colorbar
+        plt.colorbar(mappable)
     
     plt.subplots_adjust(wspace=0.05, hspace=0.05)
     return fig
