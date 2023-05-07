46172057112a3b770097672671537d1b5b0e6834
germanFid <german.fidarov@gmail.com>
2023-05-07 08:24:33 +0300

    Add dimensions feature

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import averager
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('inputfile', help='input csv file')
+parser.add_argument('inputfile', help='input csv file', type=str)
 
 # TODO: add x, y, z args
 
 parser.add_argument('--lines', '-l', help='number of header lines', type=int)
 
 parser.add_argument('--verbose', '-v', help='verbose progress', action='store_true')
+
+parser.add_argument('--dimensions', '-d', help='dimensions divided by comma', type=str)
 
 parser.add_argument('--job', '-j', help='job to do with opened Data',
                     choices=['basic2d', 'basic2d_paral', 'basic3d', 'basic3d_paral', 'gauss'],
@@ -29,6 +31,7 @@ DEFAULT_HEADER = 2
 DEFAULT_VERBOSE = False
 DEFAULT_RADIUS = 1
 DEFAULT_ITERATIONS = 1
+DIM_X, DIM_Y, DIM_Z = 0, 0, 0
 
 LOG_FORMAT = '> %(asctime)s %(message)s\n'
 logging.basicConfig(format=LOG_FORMAT)
@@ -74,6 +77,19 @@ if args.lines:
 if args.verbose:
     DEFAULT_VERBOSE = True
 
+if args.dimensions:
+    dims = str(args.dimensions).split(',')
+    if len(dims) not in [2, 3]:
+        logger.error("Incorrect number of dimensions!")
+        exit()
+
+    else:
+        DIM_X, DIM_Y = dims[0], dims[1]
+
+        if len(dims) == 3:
+            DIM_Z = dims[2]
+        
+
 if args.radius:
     DEFAULT_RADIUS = args.radius
 
@@ -86,7 +102,7 @@ if __name__ == '__main__':
 
     logger.warning('Averager Init Done!')
 
-    data = structures.StreamData(args.inputfile, 501, 981)
+    data = structures.StreamData(args.inputfile, DIM_X, DIM_Y, DIM_Z)
     logger.warning('File Loading Done!')
 
     if DEFAULT_VERBOSE:
