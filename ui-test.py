13883bb9243be676e16933cb3ad52d8e7f80cb5e
germanFid <german.fidarov@gmail.com>
2023-08-24 13:04:58 +0300

    add: test ui

---
+++ ui-test.py
@@ -0,0 +1,37 @@
+import customtkinter
+
+def button_callback():
+    print("button pressed")
+
+app = customtkinter.CTk()
+app.title("my app")
+# app.geometry("400x150")
+app.grid_columnconfigure((0, 1), weight=1)
+
+button = customtkinter.CTkButton(app, text="Open Data", command=button_callback)
+button.grid(row=0, column=0, padx=10, pady=10, sticky='ew', columnspan=2)
+
+header_label = customtkinter.CTkLabel(app, text='Header Lines', anchor="w")
+header_label.grid(row=1, column=0, padx=10, pady=10, sticky='ew')
+
+header_entry = customtkinter.CTkEntry(app)
+header_entry.grid(row=1, column=1, padx=10, pady=10, sticky='ew')
+
+verbosity_checkbox = customtkinter.CTkCheckBox(app, text="Increase Verbosity")
+verbosity_checkbox.grid(row=2, column=0, padx=10, pady=10, sticky='ew', columnspan=2)
+
+dim_frame = customtkinter.CTkFrame(app)
+dim_frame.grid(row=3, column=0, padx=10, pady=10, sticky='ew', columnspan=2)
+dim_frame.grid_columnconfigure((0, 1, 2), weight=1)
+
+dim_label = customtkinter.CTkLabel(dim_frame, text='Dimensions', anchor="w")
+dim_label.grid(row=0, column=0, padx=10, pady=10, sticky='ew', columnspan=3)
+
+x_entry = customtkinter.CTkEntry(dim_frame, placeholder_text='X', width=70)
+x_entry.grid(row=1, column=0, padx=10, pady=10, sticky='ew')
+y_entry = customtkinter.CTkEntry(dim_frame, placeholder_text='Y', width=70)
+y_entry.grid(row=1, column=1, padx=10, pady=10, sticky='ew')
+z_entry = customtkinter.CTkEntry(dim_frame, placeholder_text='Z', width=70)
+z_entry.grid(row=1, column=2, padx=10, pady=10, sticky='ew')
+
+app.mainloop()
