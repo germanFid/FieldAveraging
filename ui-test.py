ad380f1bbf14d524f6b02b6dc97cd923054dd746
germanFid <german.fidarov@gmail.com>
2023-08-25 19:13:46 +0300

    add: left frame

--- ui-test.py
+++ ui-test.py
@@ -8,19 +8,23 @@ app.title("my app")
 # app.geometry("400x150")
 app.grid_columnconfigure((0, 1), weight=1)
 
-button = customtkinter.CTkButton(app, text="Open Data", command=button_callback)
+left_frame = customtkinter.CTkFrame(app)
+left_frame.grid_columnconfigure((0, 1), weight=1)
+left_frame.grid(row=0, column=0, padx=10, pady=10, sticky='w')
+
+button = customtkinter.CTkButton(left_frame, text="Open Data", command=button_callback)
 button.grid(row=0, column=0, padx=10, pady=10, sticky='ew', columnspan=2)
 
-header_label = customtkinter.CTkLabel(app, text='Header Lines', anchor="w")
+header_label = customtkinter.CTkLabel(left_frame, text='Header Lines', anchor="w")
 header_label.grid(row=1, column=0, padx=10, pady=10, sticky='ew')
 
-header_entry = customtkinter.CTkEntry(app)
+header_entry = customtkinter.CTkEntry(left_frame)
 header_entry.grid(row=1, column=1, padx=10, pady=10, sticky='ew')
 
-verbosity_checkbox = customtkinter.CTkCheckBox(app, text="Increase Verbosity")
+verbosity_checkbox = customtkinter.CTkCheckBox(left_frame, text="Increase Verbosity")
 verbosity_checkbox.grid(row=2, column=0, padx=10, pady=10, sticky='ew', columnspan=2)
 
-dim_frame = customtkinter.CTkFrame(app)
+dim_frame = customtkinter.CTkFrame(left_frame)
 dim_frame.grid(row=3, column=0, padx=10, pady=10, sticky='ew', columnspan=2)
 dim_frame.grid_columnconfigure((0, 1, 2), weight=1)
 
